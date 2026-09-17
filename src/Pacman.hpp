ce4a0904b992e82641cc6f69a1714af270b111e3
Eros <eros.delianne-le-boucher@epitech.eu>
2026-09-17 14:04:46 +0200

    Merge branch 'main' of github.com:EpitechPGE2-2025/G-OOP-400-LIL-4-1-arcade-9 into pacman

--- src/Pacman.hpp
+++ src/Pacman.hpp
@@ -8,7 +8,12 @@
 #pragma once
 
 #include "IGameModule.hpp"
+#include <chrono>
 #include <vector>
+
+#define TILE_SIZE 15
+#define MAP_WIDTH 28
+#define MAP_HEIGHT 31
 
 namespace arcade {
     class Pacman : public IGameModule {
@@ -19,6 +24,60 @@ namespace arcade {
             void pause() override;
             void stop() override;
         private:
-            std::vector<IRect *> _tiles;
+            int _score = 0;
+            size_t _pacmanStartX = 13;
+            size_t _pacmanStartY = 17;
+            void loadMap(IDisplayModule *display);
+            void displayPacman(IDisplayModule *display);
+            void updateDirection(EEvent event);
+            void movePacman();
+            IRect *_rectBase;
+            IRect *_rectScore;
+            IRect *_pacman;
+            struct Tile {
+                IRect *rect;
+                bool visible;
+            };
+            std::vector <Tile> _map;
+            Size _windowSize;
+            Bounds _rectBounds;
+            int _dirX = 1;
+            int _dirY = 0;
+            std::chrono::steady_clock::time_point _lastMove = std::chrono::steady_clock::now();
+            std::vector<std::string> map = {
+                "############################",
+                "#............##.......0....#",
+                "#.####.#####.##.#####.####.#",
+                "#.####.#####.##.#####.####.#",
+                "#.####.#####.##.#####.####.#",
+                "#...0......................#",
+                "#.####.##.########.##.####.#",
+                "#.####.##.########.##.####.#",
+                "#......##....##....##......#",
+                "######.#####.##.#####.######",
+                "######.#####.##.#####.######",
+                "######.##..........##.######",
+                "######.##.########.##.######",
+                "######.##.########.##.######",
+                "#.........########....0....#",
+                "######.##.########.##.######",
+                "######.##.########.##.######",
+                "######.##..........##.######",
+                "######.##.########.##.######",
+                "######.##.########.##.######",
+                "#............##............#",
+                "#.####.#####.##.#####.####.#",
+                "#.####.#####.##.#####.####.#",
+                "#...##................##...#",
+                "###.##.##.########.##.##.###",
+                "###.##.##.########.##.##.###",
+                "#...0..##....##....##......#",
+                "#.##########.##.##########.#",
+                "#.##########.##.##########.#",
+                "#..........................#",
+                "############################",
+            };
+            std::vector<std::vector<IRect *>> _gumMap;
+            bool _paused = false;
     };
 }
