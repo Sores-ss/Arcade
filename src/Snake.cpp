5f7fb2e699a2cd3036e7661879271bff77b26388
Eros <eros.delianne-le-boucher@epitech.eu>
2026-09-17 14:04:07 +0200

    feat(snake init): added of snake init

--- src/Snake.cpp
+++ src/Snake.cpp
@@ -38,7 +38,29 @@ extern "C" {
 namespace arcade {
     void Snake::run(IDisplayModule *display)
     {
-        (void)display;
+        const std::size_t rectSize = 50;
+        const std::size_t mapX = 650;
+        const std::size_t mapY = 300;
+        const std::size_t mapWidth = 10;
+        const std::size_t mapHeight = 10;
+        bool mapInit = false;
+
+        if (!mapInit) {
+            for (std::size_t i = 0; i < mapHeight; i++) {
+                for (std::size_t j = 0; j < mapWidth; j++) {
+                    IRect *tile = display->createRect(rectSize, rectSize, mapX + i * rectSize,
+                        mapY + j * rectSize);
+                    if ((i + j) % 2 == 0)
+                        display->setTexture(tile, {"", 10, 150, 40, 255});
+                    else
+                        display->setTexture(tile, {"", 35, 180, 55, 255});
+                    _tiles.push_back(tile);
+                }
+            }
+            mapInit = true;
+        }
+        for (IRect *tile : _tiles)
+            display->displayRect(tile);
     }
 
     void Snake::pause()
